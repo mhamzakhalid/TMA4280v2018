c555a180ea760e8c20b0fe4010479fec6e5cfde8
Muhammad Hamza Khalid <37152213+mhamzakhalid@users.noreply.github.com>
2018-05-07 23:05:05 +0200

    Changes in meshes improvement

--- P2b/test/1dmesh.cpp
+++ P2b/test/1dmesh.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include <stdio.h>
 #include <cmath>
 #include <vector>
@@ -8,14 +9,13 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-
-    int rank, size;
-
-    MPI_Init(&argc, &argv);
-
+    MPI_Init(&argc, &argv); 
+    int size,rank; 
+    MPI_Datatype stype; 
+     
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+    
     int n;
 
     if(rank == 0){
@@ -24,34 +24,54 @@ int main(int argc, char** argv) {
 
     MPI_Bcast(&n,1,MPI_INT, 0, MPI_COMM_WORLD);
 
-    int EPP = n/size; // Elements per processor
-    double v_local[EPP][2];
-    int v[n][2];
-    double v_global[n]={};
-    double meshStep = 1./n;
-    int globalv[n][2]={};
+//**************** Generating the mesh *********************//
+    int EPP = n/size; 			  // Elements per processor
+    double localtopology[EPP][2]={};  	  //Connectivity Matrix "C(K,V)"   
+    double v_local[size][EPP+1]={};       //Vector array with information of ghost entries
+    double v_global[size][EPP+1]={};
+    double meshstep=1./n;
+    double localgeometry[n+1];  	  //Setting Geometry  
+    for(int i=rank*EPP;i<rank*EPP+EPP;i++){
+      
+	localtopology[i][0]=i;
+	localtopology[i][1]=i+1; 
+        localgeometry[i]=i*meshstep;
+      
+// Matrix that provides information of shared and ghost
+     if(rank==0){
+                for(int j=0;j<=EPP;j++){
+			v_local[rank][j]=j;             
+					}
+                    } 
+      else{  
+             for(int j=0;j<EPP;j++){v_local[rank][j]=EPP*rank+j+1;}
+	 		v_local[rank][EPP]=EPP*rank;	
+	  }
+      fflush(stdout); 
+		}   
 
-    for(int i=rank*EPP;i<rank*EPP+EPP;i++){
-      v[i][0]=i;
-      v[i][1]=i+1; 
-     fflush(stdout); }   
-    
-    MPI_Gatherv(&v,n*2, MPI_INT,
-                 &globalv,n*2,MPI_INT, MPI_COMM_WORLD);
-/*    
+     MPI_Reduce(&v_local, &v_global, size*3, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+     
+
+//******************************* Printing Connectivity on each processor****************************//
    if(rank==0){
+
+	cout<<endl<<"*** This is how local topology looks like for each process ***"<<endl<<endl;
     for(int i=rank*EPP;i<rank*EPP+EPP;i++){
-        for(int j=0;j<2;j++){cout<<"v["<<i<<"]["<<j<<"]="<<v[i][j]<<" ";}
+        cout<<"Element: ("<<i<<")  Vertices:  "<<localtopology[i][0]<<"  "<<localtopology[i][1]<<"  ";
 					cout<<endl;   }
-     }	
-*/
-   if(rank==0){
-	for(int i=0;i<n;i++){
-        for(int j=0;j<2;j++){cout<<"v["<<i<<"]["<<j<<"]="<<globalv[i][j]<<" ";}
-					cout<<endl;   }
+
+
+//******************************* Printing Vector Class that provides information ********************************//
+
+   //if(rank==0){
+                cout <<endl<<"*** Printing vector class, with information of shared entries ***"<<endl<<endl;
+		for (int i=0;i< size;i++){
+        		for(int j=0;j<=EPP;j++){ cout<<"  "<<v_global[i][j]<<" ";}
+			cout<<"   ***Process "<<i<<"   Shared: "<<v_global[i][EPP]<<endl;
+		}		     		
      }			
 
-    
+   cout<<endl;
     MPI_Finalize();
-
 }
